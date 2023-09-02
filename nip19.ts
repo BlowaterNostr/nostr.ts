402ce56e17c52de401ed37d28891826b8dc0f703
Sherry <26760878+shaibearary@users.noreply.github.com>
2023-09-02 21:22:14 +0800

    NIP 19: naddr (#22)

--- nip19.ts
+++ nip19.ts
@@ -1,5 +1,6 @@
 import { utils } from "./vendor/esm.sh/v106/@noble/secp256k1@1.7.1/es2022/secp256k1.js";
 import { bech32 } from "./scure.js";
+import { utf8Decode, utf8Encode } from "./ende.ts";
 
 export class NoteID {
     static FromBech32(id: string): NoteID | Error {
@@ -47,4 +48,75 @@ function toHex(bech: string) {
     const code = bech32.decode(bech, 1500);
     const data = new Uint8Array(bech32.fromWords(code.words));
     return utils.bytesToHex(data);
+}
+type TLV = { [t: number]: Uint8Array[] };
+function encodeTLV(tlv: TLV): Uint8Array {
+    let entries: Uint8Array[] = [];
+    for (const [t, vs] of Object.entries(tlv)) {
+        for (const v of vs) {
+            let entry = new Uint8Array(v.length + 2);
+            entry.set([parseInt(t)], 0);
+            entry.set([v.length], 1);
+            entry.set(v, 2);
+            entries.push(entry);
+        }
+    }
+    return utils.concatBytes(...entries);
+}
+function parseTLV(data: Uint8Array): TLV | Error {
+    let result: TLV = {};
+    let rest = data;
+    while (rest.length > 0) {
+        let t = rest[0];
+        let l = rest[1];
+        if (!l) return new Error(`malformed TLV ${t}`);
+        let v = rest.slice(2, 2 + l);
+        rest = rest.slice(2 + l);
+        if (v.length < l) return new Error(`not enough data to read on TLV ${t}`);
+        result[t] = result[t] || [];
+        result[t].push(v);
+    }
+    return result;
+}
+export type AddressPointer = {
+    identifier: string;
+    pubkey: string;
+    kind: number;
+    relays?: string[];
+};
+
+// https://github.com/nostr-protocol/nips/blob/master/19.md#shareable-identifiers-with-extra-metadata
+export class NostrAddress {
+    encode(): string | Error {
+        let kind = new ArrayBuffer(4);
+        new DataView(kind).setUint32(0, this.addr.kind, false);
+
+        let data = encodeTLV({
+            0: [utf8Encode(this.addr.identifier)],
+            1: (this.addr.relays || []).map((url) => utf8Encode(url)),
+            2: [utils.hexToBytes(this.addr.pubkey)],
+            3: [new Uint8Array(kind)],
+        });
+
+        const words = bech32.toWords(data);
+        return bech32.encode("naddr", words, 1500);
+    }
+    static decode(naddr: string) {
+        let { prefix, words } = bech32.decode(naddr, 1500);
+        let data = new Uint8Array(bech32.fromWords(words));
+        let tlv = parseTLV(data);
+        if (tlv instanceof Error) return tlv;
+        if (!tlv[0][0]) return new Error("missing TLV 0 for naddr");
+        if (!tlv[2][0]) return new Error("missing TLV 2 for naddr");
+        if (tlv[2][0].length !== 32) return new Error("TLV 2 should be 32 bytes");
+        if (!tlv[3][0]) return new Error("missing TLV 3 for naddr");
+        if (tlv[3][0].length !== 4) return new Error("TLV 3 should be 4 bytes");
+        return new NostrAddress({
+            identifier: utf8Decode(tlv[0][0]),
+            pubkey: utils.bytesToHex(tlv[2][0]),
+            kind: parseInt(utils.bytesToHex(tlv[3][0]), 16),
+            relays: tlv[1] ? tlv[1].map((d) => utf8Decode(d)) : [],
+        });
+    }
+    public constructor(public readonly addr: AddressPointer) {}
 }
