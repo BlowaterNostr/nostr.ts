0dfaf598c7d57f1948a4112a7357c3a834205c32
BlowaterNostr <127284497+BlowaterNostr@users.noreply.github.com>
2023-10-28 17:04:21 +0800

    Update nostr.ts

--- cli/nostr.ts
+++ cli/nostr.ts
@@ -1,21 +1,45 @@
-import { PrivateKey } from "../key.ts";
+import { PrivateKey, PublicKey } from "../key.ts";
+import { NoteID } from "../nip19.ts";
+import { ConnectionPool } from "../relay.ts";
 
-let command: string;
-if (Deno.args.length == 0) {
-    command = "h";
-} else {
-    command = Deno.args[0].toLowerCase();
+async function main() {
+    let command: string;
+    if (Deno.args.length == 0) {
+        command = "h";
+    } else {
+        command = Deno.args[0].toLowerCase();
+    }
+
+    if (command == "keygen") {
+        const pri = PrivateKey.Generate();
+        console.log("Private Key:");
+        console.log(pri.hex);
+        console.log(pri.bech32);
+        const pub = pri.toPublicKey();
+        console.log("\nPublic Key:");
+        console.log(pub.hex);
+        console.log(pub.bech32());
+    } else if (command == "get") {
+        const id = Deno.args[1];
+        if (id == undefined) {
+            console.log("need an id, for example:");
+            console.log("nostr get key/event id/nip19");
+            return;
+        }
+        const eventID = NoteID.FromString(id);
+        const pool = new ConnectionPool();
+        const err = await pool.addRelayURL("wss://nos.lol");
+        if (err instanceof Error) {
+            return err;
+        }
+        // todo: getEventByID()
+        await pool.close();
+    } else if (["h", "help", "-h", "-help", "--help"]) {
+        console.log("nostr keygen - generate key pairs");
+    }
 }
 
-if (command == "keygen") {
-    const pri = PrivateKey.Generate();
-    console.log("Private Key:");
-    console.log(pri.hex);
-    console.log(pri.bech32);
-    const pub = pri.toPublicKey();
-    console.log("\nPublic Key:");
-    console.log(pub.hex);
-    console.log(pub.bech32());
-} else if (["h", "help", "-h", "-help", "--help"]) {
-    console.log("nostr keygen - generate key pairs");
+const err = await main();
+if (err instanceof Error) {
+    console.log(err.message);
 }
