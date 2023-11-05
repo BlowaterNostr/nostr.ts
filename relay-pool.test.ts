7a02d436029398f555b6ebdabd82ee712260538c
BlowaterNostr <127284497+BlowaterNostr@users.noreply.github.com>
2023-11-05 21:55:58 +0900

    Update relay-pool.test.ts

--- relay-pool.test.ts
+++ relay-pool.test.ts
@@ -4,12 +4,13 @@ import {
     assertNotInstanceOf,
     fail,
 } from "https://deno.land/std@0.176.0/testing/asserts.ts";
-import { NostrKind } from "./nostr.ts";
+import { InMemoryAccountContext, NostrKind } from "./nostr.ts";
 import { relays } from "./relay-list.test.ts";
 import { SingleRelayConnection, SubscriptionAlreadyExist } from "./relay-single.ts";
 import { AsyncWebSocket } from "./websocket.ts";
 import * as csp from "https://raw.githubusercontent.com/BlowaterNostr/csp/master/csp.ts";
 import { ConnectionPool, RelayAlreadyRegistered } from "./relay-pool.ts";
+import { prepareNormalNostrEvent } from "./event.ts";
 
 Deno.test("ConnectionPool close gracefully 1", async () => {
     const pool = new ConnectionPool();
@@ -178,6 +179,29 @@ Deno.test("newSub 2 times & add relay url later", async (t) => {
         const res3 = await stream2.chan.pop();
         const res4 = await stream2.chan.pop();
         // as long as it does not block
+    }
+    await pool.close();
+});
+
+Deno.test("send & get event", async (t) => {
+    const pool = new ConnectionPool();
+    const err = await pool.addRelayURLs(relays);
+    if (err && err.length > 0) {
+        console.log(err);
+        fail();
+    }
+    {
+        const event = await prepareNormalNostrEvent(InMemoryAccountContext.Generate(), {
+            kind: NostrKind.CONTACTS,
+            content: "",
+        });
+        const err = await pool.sendEvent(event);
+        if (err) fail(err.message);
+
+        const e = await pool.getEvent(event.id);
+        if (e instanceof Error) fail(e.message);
+
+        assertEquals(e, event);
     }
     await pool.close();
 });
