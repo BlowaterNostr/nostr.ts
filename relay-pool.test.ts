c1a1e2451d58b28fdb6ed0a121700dd70e3cdc7a
BlowaterNostr <127284497+BlowaterNostr@users.noreply.github.com>
2023-11-17 22:02:24 +0800

    +

--- relay-pool.test.ts
+++ relay-pool.test.ts
@@ -1,9 +1,4 @@
-import {
-    assertEquals,
-    assertInstanceOf,
-    assertNotInstanceOf,
-    fail,
-} from "https://deno.land/std@0.176.0/testing/asserts.ts";
+import { assertEquals, assertNotInstanceOf, fail } from "https://deno.land/std@0.176.0/testing/asserts.ts";
 import { InMemoryAccountContext, NostrKind } from "./nostr.ts";
 import { relays } from "./relay-list.test.ts";
 import { SingleRelayConnection, SubscriptionAlreadyExist } from "./relay-single.ts";
