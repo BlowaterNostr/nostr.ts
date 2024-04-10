c377106365b415a83b3fbac0099a843fb1ea8c45
Bob <dv4xd@proton.me>
2024-04-10 15:24:23 +0800

    +

--- nip9.test.ts
+++ nip9.test.ts
@@ -1,6 +1,12 @@
 import { send_deletion_event, store_deletion_event } from "./nip9-test.ts";
-import { relays } from "./relay-list.test.ts";
+import { nos, wirednet } from "./relay-list.test.ts";
 
-Deno.test("relay store deletion event", store_deletion_event(relays[1]));
+Deno.test("relay store deletion event", async () => {
+    await store_deletion_event(nos)();
+    await store_deletion_event(wirednet)();
+});
 
-Deno.test("Deletion against a strfry relay", send_deletion_event(relays[1]));
+Deno.test("Deletion against a strfry relay", async () => {
+    await send_deletion_event(nos)();
+    await send_deletion_event(wirednet)();
+});
