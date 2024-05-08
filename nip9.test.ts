5ce3347bf8a6542c5022f15e2ba07cc52cb43214
BlowaterNostr <blowater.nostr@proton.me>
2024-05-08 20:50:26 +0100

    Update nip9.test.ts

--- nip9.test.ts
+++ nip9.test.ts
@@ -1,15 +1,13 @@
 import { delete_regular_events, store_deletion_event } from "./nip9-test.ts";
-import { blowater, damus } from "./relay-list.test.ts";
-import { relayed, wirednet } from "./relay-list.test.ts";
+import { damus } from "./relay-list.test.ts";
+import { wirednet } from "./relay-list.test.ts";
 
 Deno.test("relay store deletion event", async () => {
     await store_deletion_event(damus)();
     await store_deletion_event(wirednet)();
-    await store_deletion_event(relayed)();
 });
 
 Deno.test("delete regular events", async () => {
     await delete_regular_events(damus)();
     await delete_regular_events(wirednet)();
-    await delete_regular_events(relayed)();
 });
