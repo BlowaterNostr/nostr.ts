634a9be0ca0d62a383be7c0bae9c7a33fde8ad36
Bob <dv4xd@proton.me>
2024-06-03 17:29:53 +0800

    prepare reaction event

---
+++ nip25.ts
@@ -0,0 +1,29 @@
+import {NostrAccountContext, NostrEvent, NostrKind, Tag} from "./nostr.ts";
+import {prepareNormalNostrEvent} from "./event.ts";
+import * as nostr from "./nostr.ts";
+
+async function prepareReactionEvent(
+  author: NostrAccountContext,
+  args: {
+    content: string,
+    targetEvent: nostr.NostrEvent,
+  },
+): Promise<NostrEvent<NostrKind.REACTION> | Error> {
+  const { content, targetEvent } = args;
+
+  // https://github.com/nostr-protocol/nips/blob/master/25.md#tags
+  // There is currently no need to support replaceable events
+  const tags: Tag[] = [
+    ["e", targetEvent.id],
+    ["p", targetEvent.pubkey],
+    ["k", targetEvent.kind.toString()]
+  ];
+  return prepareNormalNostrEvent(
+    author,
+    {
+      kind: NostrKind.REACTION,
+      content,
+      tags,
+    },
+  );
+}
