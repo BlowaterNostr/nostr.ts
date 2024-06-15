5574dbf3c1863d2697a2367ecaca437979d93bdf
BlowaterNostr <blowater.nostr@proton.me>
2024-06-15 17:01:45 +0800

    fix Signer V2 backward compatibility

--- space-member.ts
+++ space-member.ts
@@ -1,9 +1,9 @@
-import { Kind_V2, Signer, SpaceMember } from "./nostr.ts";
+import { Kind_V2, Signer_V2, SpaceMember } from "./nostr.ts";
 import { InvalidKey, PublicKey } from "./key.ts";
 import { RFC3339 } from "./_helper.ts";
 import { format } from "https://deno.land/std@0.224.0/datetime/format.ts";
 
-export function prepareSpaceMember(author: Signer, member: string): Error | Promise<SpaceMember> {
+export function prepareSpaceMember(author: Signer_V2, member: string): Error | Promise<SpaceMember> {
     const pubkey = PublicKey.FromString(member);
     if (pubkey instanceof InvalidKey) {
         return new Error(pubkey.message);
