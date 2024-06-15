612500a2eccd13f0ac10e11a4365f50feb15485a
Water Blower <127284497+BlowaterNostr@users.noreply.github.com>
2024-06-15 16:52:23 +0800

    fix info stream (#90)

--- nip11.ts
+++ nip11.ts
@@ -1,17 +1,23 @@
 export async function getRelayInformation(url: string) {
+    const httpURL = new URL(url);
+    httpURL.protocol = httpURL.protocol == "wss:" ? "https" : "http";
+
+    let res: Response;
     try {
-        const httpURL = new URL(url);
-        httpURL.protocol = httpURL.protocol == "wss:" ? "https" : "http";
-        const res = await fetch(httpURL, {
+        res = await fetch(httpURL, {
             headers: {
                 "accept": "application/nostr+json",
             },
         });
+    } catch (e) {
+        return e as TypeError;
+    }
 
-        if (!res.ok) {
-            return new Error(`Faild to get detail, ${res.status}: ${await res.text()}`);
-        }
+    if (!res.ok) {
+        return new Error(`Faild to get detail, ${res.status}: ${await res.text()}`);
+    }
 
+    try {
         const detail = await res.text();
         const info = JSON.parse(detail) as RelayInformation;
         if (!info.icon) {
