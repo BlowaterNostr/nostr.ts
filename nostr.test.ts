f4ed9e878a4c1a1d24b2fe4dbc3045d091e9e364
BlowaterNostr <blowater.nostr@proton.me>
2024-09-18 13:07:43 +0800

    extend getTags

--- nostr.test.ts
+++ nostr.test.ts
@@ -1,8 +1,10 @@
 import { assertEquals } from "@std/assert";
-import { InMemoryAccountContext } from "./nostr.ts";
+import { getTags, InMemoryAccountContext, NostrEvent, NostrKind } from "./nostr.ts";
 import { RFC3339 } from "./_helper.ts";
 import { format } from "@std/datetime";
 import { verify_event_v2 } from "./v2.ts";
+import { prepareNostrEvent } from "./event.ts";
+import { PrivateKey, PublicKey } from "./key.ts";
 
 Deno.test("verify event v2", async () => {
     const ctx = InMemoryAccountContext.Generate();
@@ -14,3 +16,26 @@ Deno.test("verify event v2", async () => {
     const ok = await verify_event_v2(event);
     assertEquals(ok, true);
 });
+
+Deno.test("getTags", async () => {
+    const d = PrivateKey.Generate().hex
+    const e = PrivateKey.Generate().hex
+    const p = PrivateKey.Generate().hex
+    const event = await prepareNostrEvent(InMemoryAccountContext.Generate(), {
+        kind: NostrKind.TEXT_NOTE,
+        content: "",
+        tags: [
+            ["d", d],
+            ["e", e],
+            ["p", p],
+            ["client", "Deno"]
+        ]
+    }) as NostrEvent
+    const tags = getTags(event)
+    assertEquals(tags, {
+        e: [e],
+        p: [p],
+        d: d,
+        client: "Deno"
+    })
+})
