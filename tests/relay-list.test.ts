b23b8ef307cc26e6cc7cc52381d8071354dc95e8
BlowaterNostr <blowater.nostr@proton.me>
2024-09-19 13:03:20 +0800

    +

--- tests/relay-list.test.ts
+++ tests/relay-list.test.ts
@@ -2,9 +2,10 @@ export const damus = "wss://relay.damus.io";
 export const nos = "wss://nos.lol";
 export const wirednet = "wss://relay.nostr.wirednet.jp";
 export const blowater = "wss://blowater.nostr1.com";
+export const satlantis = "wss://relay.satlantis.io/";
 
 export const relays = [
-    wirednet,
     damus,
     nos,
+    satlantis,
 ];
