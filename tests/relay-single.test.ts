88bdb7cd9c4abecce935a194faf55f35e19b301d
BlowaterNostr <blowater.nostr@proton.me>
2024-11-03 23:32:27 +0800

    Update relay-single.test.ts

--- tests/relay-single.test.ts
+++ tests/relay-single.test.ts
@@ -1,6 +1,7 @@
-import { wirednet } from "./relay-list.test.ts";
+import { sleep } from "@blowater/csp";
 import { nos } from "./relay-list.test.ts";
-import { blowater, damus, relays, satlantis } from "./relay-list.test.ts";
+import { wirednet } from "./relay-list.test.ts";
+import { damus, relays, satlantis } from "./relay-list.test.ts";
 import {
     close_sub_keep_reading,
     get_correct_kind,
@@ -24,28 +25,31 @@ Deno.test("SingleRelayConnection newSub & close", async () => {
 
 Deno.test("Single Relay Connection", async (t) => {
     const relay = {
-        ws_url: damus,
+        ws_url: wirednet,
     };
     await t.step("SingleRelayConnection subscription already exists", sub_exits(relay.ws_url));
+    await sleep(100);
     await t.step(
         "SingleRelayConnection: close subscription and keep reading",
         close_sub_keep_reading(relay.ws_url),
     );
+    await sleep(100);
     await t.step("send event", send_event(relay.ws_url));
+    await sleep(100);
     await t.step("get_correct_kind", get_correct_kind(relay.ws_url));
-
-    await t.step("multiple filters", newSub_multiple_filters(relay.ws_url));
-
+    await sleep(100);
     await t.step("limit", limit(relay.ws_url));
-
+    await sleep(100);
     await t.step("no_event", no_event(relay.ws_url));
-
+    await sleep(100);
     await t.step("two_clients_communicate", two_clients_communicate(relay.ws_url));
+});
 
-    await t.step("get_event_by_id", async () => {
-        await get_event_by_id(relay.ws_url)();
-        await get_event_by_id(nos)();
-    });
+Deno.test("multiple filters", newSub_multiple_filters(damus));
+
+Deno.test("get_event_by_id", async () => {
+    await get_event_by_id(wirednet)();
+    await get_event_by_id(damus)();
 });
 
 Deno.test("get replaceable event", async () => {
